8c3bab13af76eea9c8ce3c60bf30a63e43f4ee92
rafiqwe <rmlmanik502@gmail.com>
2025-06-24 20:02:55 +0600

    Create user routes

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { body } = require("express-validator");
+const router = express.Router();
+const userController = require("../controllers/user.controller");
+
+// Assuming userController.register is the handler function
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characters long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  userController.registerUser
+);
+
+module.exports = router;
