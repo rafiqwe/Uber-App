a030ebe96e09a0d731a7aee159508f835ed20575
rafiqwe <rmlmanik502@gmail.com>
2025-06-24 17:15:42 +0600

    first commit

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello world');
+});
+
+module.exports = app;
