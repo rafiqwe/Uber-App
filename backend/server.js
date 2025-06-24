ec854f2e3aefff0ce210842123584e2aa21db0f4
rafiqwe <rmlmanik502@gmail.com>
2025-06-24 17:32:36 +0600

    Connect database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const app = require("./app");
 const port = process.env.PORT ;
-
+const connectDb = require('./db/db');
+connectDb();
 const server = http.createServer(app);
 
 
