01cf3ba1c93d45b63be63db47fd9430a9b681c92
rafiqwe <rmlmanik502@gmail.com>
2025-06-24 20:06:14 +0600

    Create user route and add middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
+const userRoute = require("./routes/user.routes");
+const cors = require('cors')
 
-app.get('/', (req,res) => {
-    res.send('Hello world');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
+
+app.use("/users", userRoute);
 
 module.exports = app;
