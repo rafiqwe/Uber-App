2ac468fa588ad5f3487617c22359b688e139beff
rafiqwe <rmlmanik502@gmail.com>
2025-06-25 16:10:47 +0600

    Install cookie parser

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ dotenv.config();
 const express = require("express");
 const app = express();
 const userRoute = require("./routes/user.routes");
-const cors = require('cors')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
