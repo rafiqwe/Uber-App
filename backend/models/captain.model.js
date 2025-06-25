bc0346f28b926aab553a8adc6e31c885a8806845
rafiqwe <rmlmanik502@gmail.com>
2025-06-25 18:31:36 +0600

    Create captainModel for captain

---
+++ backend/models/captain.model.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const captainSchema = mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be at least 3 characters long"],
+    },
+    lastname: {
+      type: String,
+      minlength: [3, "Last name must be at least 3 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [5, "Email must be at least 5 characters long"],
+    match: [/.+\@.+\..+/, "Please fill a valid email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false, // Prevent password from being returned in queries
+  },
+  socketId: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "inactive",
+  },
+  vehicle: {
+    color: {
+      type: String,
+      required: true,
+      minlength: [3, "Color must be at least 3 characters long"],
+    },
+    plate: {
+      type: String,
+      required: true,
+      minlength: [3, "Plate must be at least 3 characters long"],
+    },
+    capacity: {
+      type: Number,
+      required: true,
+      min: [1, "Capacity must be at least 1"],
+    },
+    vehicleType: {
+      type: String,
+      required: true,
+      enum: ["car", "motorcycle", "auto"],
+    },
+  },
+  location: {
+    lat: Number,
+    lng: Number,
+  },
+});
+
+// Generate JWT Token
+captainSchema.methods.generateAuthToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+};
+
+// Hash password before save
+captainSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+// Compare password
+captainSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const Captain = mongoose.model("Captain", captainSchema);
+module.exports = Captain;
