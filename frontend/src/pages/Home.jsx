544d1a79bad4f1a2d209d327f816d2a723e55f72
rafiqwe <rmlmanik502@gmail.com>
2025-06-26 21:30:56 +0600

    Create /  page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa6";
+
 const Home = () => {
   return (
-    <div>
-      <div className="bg-cover bg-center  bg-[url(/images/home.png)] h-screen pt-8 w-full flex justify-between flex-col bg-red-400">
-        <div>
-            <img className="w-14 ml-9 " src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png" alt="" />
-        </div>
-        <div className="bg-white py-5 px-3 pb-7">
-          <h1 className="text-2xl font-bold">Get Started with Uber</h1>
-          <Link to={'/login'} className="flex relative items-center justify-center w-full py-3 text-white text-xl bg-black rounded-2xl mt-5">Continue <span className="absolute right-8" ><FaArrowRight /></span> </Link>
-        </div>
+    <div className="min-h-screen flex flex-col justify-between bg-cover bg-center bg-[url('/images/home.png')]">
+      {/* Logo */}
+      <div className="pt-6 px-6 sm:px-10">
+        <img
+          className="w-24 sm:w-32"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
+          alt="Uber"
+        />
+      </div>
+
+      {/* Bottom Card */}
+      <div className="bg-white px-6 py-8 rounded-t-3xl shadow-xl sm:px-10">
+        <h1 className="text-2xl sm:text-3xl font-bold text-gray-900">
+          Get Started with Uber
+        </h1>
+
+        <Link
+          to={"/login"}
+          className="flex items-center justify-center gap-2 mt-6 w-full py-3 text-white text-lg sm:text-xl bg-black rounded-xl hover:bg-gray-900 transition"
+        >
+          Continue
+          <FaArrowRight className="text-base sm:text-lg" />
+        </Link>
       </div>
     </div>
   );
