ab8b0ba7c0dbe89a9de6c7a28499740d4f50122c
rafiqwe <rmlmanik502@gmail.com>
2025-06-27 20:45:27 +0600

    Create user logout route and captain logout or captain home. wrap user home page in user protected wraper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/home";
+import Start from "./pages/Start";
 import UserLogin from "./pages/userLogin";
 import UserRegister from "./pages/userRegister";
 import CaptainLogin from "./pages/captainLogin";
 import CaptainRegister from "./pages/captainRegister";
+import Home from "./pages/home";
+import UserProtectedWraper from "./pages/UserProtectedWraper";
+import UserLogOut from "./pages/UserLogOut";
+import CaptainHome from "./pages/CaptainHome";
+import CaptainProtectedWraper from "./pages/CaptainProtectedWraper";
+import CaptainLogout from "./pages/CaptainLogout";
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Start />,
+    },
+    {
+      path: "/home",
+      element: (
+        <UserProtectedWraper>
+          <Home />
+        </UserProtectedWraper>
+      ),
     },
     {
       path: "/login",
@@ -26,6 +40,22 @@ const App = () => {
     {
       path: "/captain-register",
       element: <CaptainRegister />,
+    },
+    {
+      path: "/users-logout",
+      element: <UserLogOut />,
+    },
+    {
+      path: "/captain-logout",
+      element: <CaptainLogout />,
+    },
+    {
+      path: "/captain-home",
+      element: (
+        <CaptainProtectedWraper>
+          <CaptainHome />
+        </CaptainProtectedWraper>
+      ),
     },
   ]);
 
