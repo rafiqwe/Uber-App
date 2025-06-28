1899b8cd0adfeb6b94353324f9d2add385258c2b
rafiqwe <rmlmanik502@gmail.com>
2025-06-28 20:10:11 +0600

    Create Looking For Driver components for user

---
+++ frontend/src/components/LookingForDriver.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { FaAngleDown } from "react-icons/fa";
+import { FaLocationDot, FaMapLocationDot } from "react-icons/fa6";
+import { RiCurrencyLine } from "react-icons/ri";
+
+const LookingForDriver = ({ setvehileFound, downIcon2 }) => {
+  return (
+    <div>
+      <div className="absolute top-7 right-5">
+        <FaAngleDown
+          onClick={() => {
+            setvehileFound(false);
+          }}
+          ref={downIcon2}
+          className="text-lg"
+        />
+      </div>
+      <h1 className="text-2xl font-bold mb-5">Looking For Driver</h1>
+      <div className="flex justify-center items-center gap-2 flex-col ">
+        <div>
+          <img
+            className="h-30"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1555367310/assets/30/51e602-10bb-4e65-b122-e394d80a9c47/original/Final_UberX.png"
+            alt="car img"
+          />
+        </div>
+        <div className="w-full">
+          <div className="flex mb-3 items-center gap-4 w-full border-b-1 p-2  border-gray-400">
+            <div className="w-7 h-7 flex items-center justify-center rounded-full">
+              <FaLocationDot className="text-xl" />
+            </div>
+            <div>
+              <h2 className="font-semibold text-lg">542/42-R</h2>
+              <h2 className="text-gray-500 text-sm">
+                North Mawna sheepur, Gazipur
+              </h2>
+            </div>
+          </div>
+          <div className="flex mb-3 items-center gap-4 w-full border-b-1 p-2  border-gray-400">
+            <div className="w-7 h-7 flex items-center justify-center rounded-full">
+              <FaMapLocationDot className="text-xl" />
+            </div>
+            <div>
+              <h2 className="font-semibold text-lg">542/42-R</h2>
+              <h2 className="text-gray-500 text-sm">
+                North Mawna sheepur, Gazipur
+              </h2>
+            </div>
+          </div>
+          <div className="flex mb-3 items-center gap-4 w-full p-2 ">
+            <div className="w-7 h-7 flex items-center justify-center rounded-full">
+              <RiCurrencyLine className="text-xl" />
+            </div>
+            <div>
+              <h2 className="font-semibold text-lg">192.30 Tk</h2>
+              <h2 className="text-gray-500 text-sm">Cash Cash</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LookingForDriver;
