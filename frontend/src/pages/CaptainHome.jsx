d16aef24d981b47ee945efde6f65e5fb2bf4f0ea
rafiqwe <rmlmanik502@gmail.com>
2025-06-29 16:56:08 +0600

    CaptainConfirmRidePopup, CaptainDetails,CaptianRidePopup  .add all these ingredients at home page step by step

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,11 +1,40 @@
-import { FaMapLocationDot } from "react-icons/fa6";
-import { RiCurrencyLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { IoIosLogOut } from "react-icons/io";
-import { MdOutlineAvTimer } from "react-icons/md";
-import { IoMdSpeedometer } from "react-icons/io";
-import { PiNotePencilBold } from "react-icons/pi";
+import CaptainDetails from "../components/CaptainDetails";
+import CaptainRidePopup from "../components/CaptainRidePopup";
+import { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import CaptainConfirmRidePopup from "../components/CaptainConfirmRidePopup";
 const CaptainHome = () => {
+  const [ridePopupPanel, setridePopupPanel] = useState(true);
+  const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false);
+  const ridePopupPanelRef = useRef(null);
+  const confirmRidePopupPanelRef = useRef(null);
+  useGSAP(() => {
+    if (ridePopupPanel) {
+      gsap.to(ridePopupPanelRef.current, {
+        transform: "translateY(0)",
+      });
+    } else {
+      gsap.to(ridePopupPanelRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [ridePopupPanel]);
+
+  useGSAP(() => {
+    if (confirmRidePopupPanel) {
+      gsap.to(confirmRidePopupPanelRef.current, {
+        transform: "translateY(0)",
+      });
+    } else {
+      gsap.to(confirmRidePopupPanelRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [confirmRidePopupPanel]);
+
   return (
     <div className="h-screen overflow-hidden ">
       <div className="absolute w-full flex items-center justify-between px-4 mt-2">
@@ -21,41 +50,33 @@ const CaptainHome = () => {
           <IoIosLogOut className="font-bold text-lg " />
         </Link>
       </div>
-      <div className="h-1/2">
+      <div className="h-3/5">
         <img
           className="h-full w-full object-cover"
           src="https://i2-prod.mylondon.news/article16106961.ece/ALTERNATES/s615/2_Uber-pink-cars.jpg"
           alt=""
         />
       </div>
-      <div className="h-1/2 p-2">
-        <div>
-          <div>
-            <img src="" alt="" />
-            <h2>Muhammad Rabbi</h2>
-          </div>
-          <div>
-            <h4>320.10</h4>
-            <p>Earned</p>
-          </div>
-        </div>
-        <div className="flex mt-8 items-center justify-between">
-          <div className="text-center ">
-            <MdOutlineAvTimer className="text-2xl text-center font-thin " />
-            <h2 className="text-lg font-medium">10.2</h2>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-          <div className="text-center">
-            <IoMdSpeedometer />
-            <h2 className="text-lg font-medium">10.2</h2>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-          <div className="text-center">
-            <PiNotePencilBold />
-            <h2 className="text-lg font-medium">10.2</h2>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-        </div>
+      <div className="h-2/5 p-6">
+        <CaptainDetails />
+      </div>
+      <div
+        ref={ridePopupPanelRef}
+        className="fixed z-10  bottom-0 rounded-t-3xl  bg-white px-2 py-8 w-full "
+      >
+        <CaptainRidePopup
+          setridePopupPanel={setridePopupPanel}
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+        />
+      </div>
+      <div
+        ref={confirmRidePopupPanelRef}
+        className="fixed z-10  bottom-0 h-screen rounded-t-3xl  bg-white px-2 py-8 w-full "
+      >
+        <CaptainConfirmRidePopup
+          setridePopupPanel={setridePopupPanel}
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+        />
       </div>
     </div>
   );
