947226547fa8cfd5cbdec4bf3719f340c68d72bb
rafiqwe <rmlmanik502@gmail.com>
2025-07-01 22:06:08 +0600

    Created get fare controller

--- backend/controllers/ride.controller.js
+++ backend/controllers/ride.controller.js
@@ -21,3 +21,23 @@ module.exports.createRide = async (req, res) => {
       .json({ message: "Internal server error", error: error.message });
   }
 };
+
+module.exports.getFare = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const { pickup, destination, vehicleType } = req.query;
+    const fare = await rideService.getFare(pickup, destination, vehicleType);
+    
+    res.status(200).json({ fare: fare.toFixed(2) });
+  } catch (error) {
+    console.error("Error fetching fare:", error);
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
