dffb29a6987eef339a85f4551b422df496c22cf0
rafiqwe <rmlmanik502@gmail.com>
2025-07-06 12:46:50 +0600

    Add socket.js on server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const http = require("http");
 const app = require("./app");
-const port = process.env.PORT ;
-const connectDb = require('./db/db');
+const port = process.env.PORT;
+const connectDb = require("./db/db");
+const { initializeSocket } = require("./socket"); // <-- Import
+
 connectDb();
 const server = http.createServer(app);
 
+initializeSocket(server); // <-- Initialize socket.io
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
