be3304e0250e5180418955f2b60fb8c516e8b3a7
rafiqwe <rmlmanik502@gmail.com>
2025-07-07 22:40:24 +0600

    Add captainContext or SocketProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import UserContext from "./context/UserContext.jsx";
+import CaptainContext from "./context/CaptainContext.jsx";
+import SocketProvider from "./context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UserContext>
-      <App />
-    </UserContext>
+    <SocketProvider>
+      <CaptainContext>
+        <UserContext>
+          <App />
+        </UserContext>
+      </CaptainContext>
+    </SocketProvider>
   </StrictMode>
 );
