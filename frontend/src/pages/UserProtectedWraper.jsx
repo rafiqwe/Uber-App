fa05b05bc043fbb0f87718d5d10d962039785b9c
rafiqwe <rmlmanik502@gmail.com>
2025-07-09 22:29:27 +0600

    Add User context in User protected wraper component

--- frontend/src/pages/UserProtectedWraper.jsx
+++ frontend/src/pages/UserProtectedWraper.jsx
@@ -1,34 +1,34 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
 
 const UserProtectedWraper = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const token = localStorage.getItem("token");
+  const { setuser } = useContext(UserDataContext);
 
   const navigate = useNavigate();
   useEffect(() => {
     if (!token) {
       navigate("/login");
     }
-  }, [navigate]);
-
-  axios
-    .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((response) => {
-      if (response.status === 200) {
-        setLoading(false);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      localStorage.removeItem("token");
-      navigate("/login");
-    });
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setuser(response.data);
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   if (loading) return <div className="text-center mt-10">Loading...</div>;
 
